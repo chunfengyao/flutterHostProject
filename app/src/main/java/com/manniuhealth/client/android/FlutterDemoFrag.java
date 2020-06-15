3dd47c9fe66535b8f8a8668340ea7e17dd8b3c37
姚春锋 <yaochunfeng@wondersgroup.com>
2020-06-15 19:14:49 +0800

    add{添加Demo页面，以及测试的视图。}

---
+++ app/src/main/java/com/manniuhealth/client/android/FlutterDemoFrag.java
@@ -0,0 +1,15 @@
+package com.manniuhealth.client.android;
+/* * *
+ * pkg name:     com.manniuhealth.client.android
+ * created by:   yaochunfeng
+ * on:           2020/6/15 18:51
+ * Des:          Used to .
+ * Email:        yaochunfeng@wondersgroup.com
+ *
+ * * */
+
+import io.flutter.embedding.android.FlutterFragment;
+
+public class FlutterDemoFrag extends FlutterFragment {
+
+}
--- app/src/main/java/com/manniuhealth/client/android/MainActivity.java
+++ app/src/main/java/com/manniuhealth/client/android/MainActivity.java
@@ -1,14 +1,47 @@
 package com.manniuhealth.client.android;
 
+import androidx.annotation.Keep;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button startNewFlutterPageBtn;
+    private Button createNewFlutterViewBtn;
+    private LinearLayout flutterViewContainer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startNewFlutterPageBtn = findViewById(R.id.startNewFlutterPageBtn);
+        createNewFlutterViewBtn = findViewById(R.id.createNewFlutterViewBtn);
+        flutterViewContainer = findViewById(R.id.flutterViewContainer);
+
     }
+
+    /**
+     * 点击事件
+     * @param view
+     */
+    @Keep
+    public void startNewFlutterPageBtnClick(View view){
+        FlutterDemoActivity.start(this);
+    }
+
+    /**
+     * 点击事件
+     * @param view
+     */
+    @Keep
+    public void createNewFlutterViewBtnClick(View view){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flutterViewContainer, new FlutterDemoFrag())
+                .commit();
+    }
+
 }
